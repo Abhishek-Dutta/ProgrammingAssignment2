749ab6e56c3828c57bba5d930771c99ed27da1c9
/Users/abhishekdutta/Downloads/Git <abhishekdutta@abhisheks.home>
2014-05-25 17:13:56 -0400

    R Programming Assignment 2
    R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the value of the matrix
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  ## get the value of the matrix
+  get <- function() x
+  
+  ## set the inverse of the matrix
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  
+  ## get the inverse of the matrix
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -12,4 +28,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ## get the inverse of the matrix   
+  s <- x$getsolve()
+  
+  ## check if there is the matrix   
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  ## if not: get the inverse of the matrix   
+  data <- x$get()
+  s <- solve(data, ...)
+  
+  ## set the inverse of the matrix 
+  x$setsolve(s)
+  s
 }
